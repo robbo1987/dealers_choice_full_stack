f7d85594b0dbb413783d5b5ea6f729312087c30e
Robert Albenda <ralbenda@gmail.com>
2022-03-19 11:40:37 -0400

    front end up

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import React from 'react';
-import { render } from 'react-dom';
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
 
-render(<hr />, document.querySelector('#root'));
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artists: [],
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+          <h1>
+              Robby's Italian Fine Art Museum
+          </h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));

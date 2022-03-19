e22328416496110b191fc029c07b3fe84cbd7a6f
Robert Albenda <ralbenda@gmail.com>
2022-03-19 12:10:18 -0400

    front end looking better

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 class App extends React.Component {
   constructor() {
     super();
@@ -11,13 +10,23 @@ class App extends React.Component {
     };
   }
 
+  async componentDidMount() {
+    const artistsData = (await axios.get("/api/artists")).data;
+    console.log("this is the artist data", artistsData);
+    this.setState( {artists: artistsData})
+  }
 
   render() {
+    console.log("this is the state", this.state);
     return (
       <div>
-          <h1>
-              Robby's Italian Fine Art Museum
-          </h1>
+        <h1>Robby's Italian Fine Art Museum</h1>
+        <ul>
+          {this.state.artists.map((artist) => {
+            return <li>{artist.name}</li>;
+          })}
+        </ul>
+        <h3>hello world</h3>
       </div>
     );
   }

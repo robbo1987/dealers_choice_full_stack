3810d3a6ebae9452949ccfb93722475068a8a0a1
Robert Albenda <ralbenda@gmail.com>
2022-03-22 14:31:16 -0400

    working on it

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import Artists from "./Artists.react";
 import Museums from "./Museums.react";
-import Nav from "./Nav.react";
-import AddArtist from "./AddArtist.react"
+import AddArtist from "./AddArtist.react";
 import store, { loadArtists, loadMuseums } from "./store";
 import { Provider, connect } from "react-redux";
 import { HashRouter, Route } from "react-router-dom";
-
 
 const artistDetail = connect((state) => state)((props) => {
   const artist = props.artists.find(
@@ -17,27 +15,26 @@ const artistDetail = connect((state) => state)((props) => {
     return null;
   }
   return (
-    <div>
-      {artist.name} was an Italian Artist from the {artist.period} Period.  He was born {artist.birthday} in Italy.
-      
-    </div>
+    <li>
+      {artist.name} was an Italian Artist from the {artist.period} Period. He
+      was born {artist.birthday} in Italy.
+    </li>
   );
 });
 
-const museumDetail = connect((state)=>state)((props) => {
+const museumDetail = connect((state) => state)((props) => {
   const museum = props.museums.find(
-    museum => museum.id === props.match.params.id*1
+    (museum) => museum.id === props.match.params.id * 1
   );
   if (!museum) {
-    return null 
+    return null;
   }
-  return(
+  return (
     <li>
       {museum.name} is located at {museum.address}
-      
     </li>
-  )
-})
+  );
+});
 const _App = connect(
   (state) => {
     return state;
@@ -62,11 +59,13 @@ const _App = connect(
           <h1>Welcome to Robby's Italian Art Tour Guide Site</h1>
           <AddArtist />
           <h2>Here is my list of "TOP TIER" Italian Artists</h2>
-          <Route path="/Artists/:id" exact component={artistDetail} />
           <Artists />
+          <Route path="/Artists/:id" exact component={artistDetail} />
+
           <h2>Museums to View These Artists and More!</h2>
-          <Route path = "/Museums/:id" exact component={museumDetail} />
+
           <Museums />
+          <Route path="/Museums/:id" exact component={museumDetail} />
         </div>
       );
     }
